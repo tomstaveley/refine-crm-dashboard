1812c7583abd8b386e87a38e054378935a6cdd0d
Tom Staveley <4049079+tomstaveley@users.noreply.github.com>
2024-03-21 18:59:31 +0000

    Project init, dashboard and companies

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,24 @@
-export { Header } from "./header";
+import UpcomingEvents from "./home/upcoming-events";
+import DealsChart from "./home/deals-chart";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+
+import DashboardTotalCountCard from "./home/total-count-card";
+import LatestActivities from "./home/latest-activities";
+
+export {
+  UpcomingEvents,
+  DealsChart,
+
+  UpcomingEventsSkeleton,
+  AccordionHeaderSkeleton,
+  KanbanColumnSkeleton,
+  ProjectCardSkeleton,
+  LatestActivitiesSkeleton,
+
+  DashboardTotalCountCard,
+  LatestActivities,
+};
